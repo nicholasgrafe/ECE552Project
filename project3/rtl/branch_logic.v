5b5a0e5662e2adf7df4efd559e0fd2033f9abbf9
James Park <truejames35@gmail.com>
2026-02-22 15:06:20 -0600

    added sign/zero extension module + dmem logic

--- project3/rtl/branch_logic.v
+++ project3/rtl/branch_logic.v
@@ -13,8 +13,8 @@ module branch_logic (
 
     always @(*) begin
         case (i_funct3)
-            3'b000: cond = i_eq             // BEQ
-            3'b001: cond = ~i_eq            // BNE
+            3'b000: cond = i_eq;            // BEQ
+            3'b001: cond = ~i_eq;           // BNE
             3'b100, 3'b110: cond = i_slt;   // BLT, BLTU
             3'b101, 3'b111: cond = ~i_slt;  // BGE, BGEU
             default: cond = 1'b0;
