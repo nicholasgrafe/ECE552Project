eb061afacd1dcb398b129085e7969f9f82d42487
unknown <nicholas.grafe@outlook.com>
2026-02-22 15:48:56 -0600

    stuff

--- project3/rtl/sign_zero_ext.v
+++ project3/rtl/sign_zero_ext.v
@@ -17,13 +17,13 @@ module sign_zero_ext (
                                                      i_dmem_rdata[31:24];
 
     // Halfword selection (aligned: offset 0 or 2)
-    assign half_val = i_byte_offset[1] ? i_dmem_rdata[31:16] : i_dmem_rdata[15:0];
+    assign hw_val = i_byte_offset[1] ? i_dmem_rdata[31:16] : i_dmem_rdata[15:0];
 
-    assign o_dmem_ext = (i_funct3 == 3'b000) ? {{24{byte_val[7]}},  byte_val}  :  // LB
-                        (i_funct3 == 3'b001) ? {{16{half_val[15]}}, half_val}  :  // LH
-                        (i_funct3 == 3'b100) ? {24'b0, byte_val}               :  // LBU
-                        (i_funct3 == 3'b101) ? {16'b0, half_val}               :  // LHU
-                                               i_dmem_rdata;                      // LW (default)
+    assign o_dmem_ext = (i_funct3 == 3'b000) ? {{24{byte_val[7]}},  byte_val}  	:  // LB
+                        (i_funct3 == 3'b001) ? {{16{hw_val[15]}}, hw_val}  	:  // LH
+                        (i_funct3 == 3'b100) ? {24'b0, byte_val}               	:  // LBU
+                        (i_funct3 == 3'b101) ? {16'b0, hw_val}               	:  // LHU
+                                               i_dmem_rdata;                       // LW (default)
 
 endmodule
 
