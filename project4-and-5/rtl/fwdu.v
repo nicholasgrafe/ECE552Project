3a25aaf6707f0124437b1338405bbd30220fa63c
James Park <truejames35@gmail.com>
2026-03-09 10:03:12 -0500

    added forwarding unit

---
+++ project4-and-5/rtl/fwdu.v
@@ -0,0 +1,13 @@
+`default_nettype none
+
+// hazard detection unit for RISC-V processor
+module fwdu (
+    // inputs
+    // outputs
+);
+
+    // insert implementation
+
+endmodule
+
+`default_nettype wire
