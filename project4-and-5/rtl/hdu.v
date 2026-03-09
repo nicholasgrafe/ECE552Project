9a5e5b25d6c39016e4720655cbf4f2089061b036
James Park <truejames35@gmail.com>
2026-03-09 10:55:40 -0500

    added forwarding logic

--- project4-and-5/rtl/hdu.v
+++ project4-and-5/rtl/hdu.v
@@ -1,6 +1,6 @@
 `default_nettype none
 
-// hazard detection unit for RISC-V processor
+// Hazard detection unit for RISC-V processor
 module hdu (
     // Source register addresses of the current instruction in ID
     input  wire [4:0] i_id_rs1,
@@ -9,24 +9,17 @@ module hdu (
     // ID/EX inputs
     input  wire       i_ex_rd_wen,
     input  wire [4:0] i_ex_rd_waddr,
+    input  wire       i_ex_dmem_ren,
 
-    // EX/MEM inputs
-    input  wire       i_mem_rd_wen,
-    input  wire [4:0] i_mem_rd_waddr,
-
-    // Asserted when a data hazard is detected
+    // Asserted on load-to-use stalls
     output wire       o_stall
 );
 
-    // Hazards from the instruction in EX (one cycle ahead of ID):
-    wire hazard_ex_rs1 = i_ex_rd_wen && (i_ex_rd_waddr != 5'd0) && (i_ex_rd_waddr == i_id_rs1);
-    wire hazard_ex_rs2 = i_ex_rd_wen && (i_ex_rd_waddr != 5'd0) && (i_ex_rd_waddr == i_id_rs2);
+    // Check if lw -> some op and rd = rs/rt
+    wire hazard_load_rs1 = i_ex_rd_wen && i_ex_dmem_ren && (i_ex_rd_waddr != 5'd0) && (i_ex_rd_waddr == i_id_rs1);
+    wire hazard_load_rs2 = i_ex_rd_wen && i_ex_dmem_ren && (i_ex_rd_waddr != 5'd0) && (i_ex_rd_waddr == i_id_rs2);
 
-    // Hazards from the instruction in MEM (two cycles ahead of ID):
-    wire hazard_mem_rs1 = i_mem_rd_wen && (i_mem_rd_waddr != 5'd0) && (i_mem_rd_waddr == i_id_rs1);
-    wire hazard_mem_rs2 = i_mem_rd_wen && (i_mem_rd_waddr != 5'd0) && (i_mem_rd_waddr == i_id_rs2);
-
-    assign o_stall = hazard_ex_rs1 | hazard_ex_rs2 | hazard_mem_rs1 | hazard_mem_rs2;
+    assign o_stall = hazard_load_rs1 | hazard_load_rs2;
 
 endmodule
 
